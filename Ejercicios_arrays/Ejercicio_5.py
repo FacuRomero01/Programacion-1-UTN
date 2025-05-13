77f238af4758a0f8529224f81fff87b5c1dd4397
Facu <romero.facundo010@gmail.com>
2025-05-13 18:59:58 -0300

    Modifiqué el código para que cumpla con la consigna

--- Ejercicios_arrays/Ejercicio_5.py
+++ Ejercicios_arrays/Ejercicio_5.py
@@ -1,51 +1,40 @@
 # Importo la función que nos devuelve el menor de los números
 from Funciones import devuelve_menor_de_los_numeros_lista
 
-def chequea_y_retorna_menores_edad(edades: list) -> list:
-    # Guardo en la variable 'edad_minima' la edad mínima de la lista de edades utilizando la función importada 'devuelve_menor_de_los_numeros'
-    edad_minima = devuelve_menor_de_los_numeros_lista(edades)
+# Defino las variables "nombres" y "edades"
 
-    # Lista vacía para almacenar los nombres de las personas con la edad mínima
-    lista_nombres_menores_edad = []
-
-    # Recorro la lista de edades
-    for i in range(len(edades)):
-        # Si encontramos una persona con la edad mínima, agregamos su nombre a la lista
-        if edades[i] == edad_minima:
-            lista_nombres_menores_edad = lista_nombres_menores_edad + [nombres[i]]
-
-    # Retorno la lista con los nombres de las personas de menor edad
-    return lista_nombres_menores_edad
-
-
-# Defino las listas de nombres y edades de las personas
 nombres = ["Ana", "Luis", "Juan", "Sol", "Roberto", "Sonia", "Ulises", "Sofia", "Maria", "Pedro", "Antonio", "Eugenia", "Soledad", "Mario", "Mariela"]
 edades = [23, 45, 34, 23, 46, 23, 45, 67, 37, 68, 25, 55, 45, 27, 43]
 
-# Llamo a la función para obtener los nombres de las personas con la edad mínima
-nombres_menores = chequea_y_retorna_menores_edad(edades)
+# Utilizo la funcion "devuelve_menor_de_los_numeros_lista" para buscar la menor edad dentro de la lista "edades" y al valor le asigno la variable "edad_minima"
+edad_minima = devuelve_menor_de_los_numeros_lista(edades)
 
-# Obtengo la edad mínima de la lista de edades
-edad_menores = devuelve_menor_de_los_numeros_lista(edades)
+# Inicializo la variable "lista_nombres_menores_edad" para almacenar los nombres de las personas con la menor edad
+lista_nombres_menores_edad = []
+
+# Uso un bucle for para ir buscando a las personas
+for i in range(len(edades)):    
+    if edades[i] == edad_minima:
+        lista_nombres_menores_edad = lista_nombres_menores_edad + [nombres[i]]
 
 # Inicializo una cadena vacía para almacenar los nombres de las personas con la edad mínima
 nombres_menores_str = ""
 
 # Recorro la lista de nombres de las personas con la edad mínima
-for i in range(len(nombres_menores)):
+for i in range(len(lista_nombres_menores_edad)):
     # Si es el último nombre de la lista, solo lo agregamos sin coma ni "y"
-    if i == len(nombres_menores) - 1:
-        nombres_menores_str += nombres_menores[i]
+    if i == len(lista_nombres_menores_edad) - 1:
+        nombres_menores_str += lista_nombres_menores_edad[i]
     # Si es el penúltimo nombre, agregamos "y" para que se lea correctamente
-    elif i == len(nombres_menores) - 2:
-        nombres_menores_str += nombres_menores[i] + " y "
+    elif i == len(lista_nombres_menores_edad) - 2:
+        nombres_menores_str += lista_nombres_menores_edad[i] + " y "
     # Para todos los demás nombres, agregamos una coma
     else:
-        nombres_menores_str += nombres_menores[i] + ", "
+        nombres_menores_str += lista_nombres_menores_edad[i] + ", "
 
 # Compruebo si solo hay una persona de menor edad, y mostramos un mensaje adecuado
-if len(nombres_menores) == 1:
-    print(f"{nombres_menores_str} es la persona de menor edad con {edad_menores} años")
+if len(lista_nombres_menores_edad) == 1:
+    print(f"{nombres_menores_str} es la persona de menor edad con {edad_minima} años")
 else:
     # Si hay más de una persona, mostramos un mensaje en plural
-    print(f"{nombres_menores_str} son las personas de menor edad con {edad_menores} años")
+    print(f"{nombres_menores_str} son las personas de menor edad con {edad_minima} años")
